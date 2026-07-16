ac51320b4106cd2acef5efc92badca01da92056e
Hiranya-07-2004 <hiranyadekkatha@gmail.com>
2026-07-16 05:37:41 +0530

    Update WebConfig.java

--- src/main/java/com/CivicPulse/CivicPulseHub/config/WebConfig.java
+++ src/main/java/com/CivicPulse/CivicPulseHub/config/WebConfig.java
@@ -10,15 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // ✅ Allows Angular to load images from the local 'uploads' folder
-        registry.addResourceHandler("/uploads/**")
+         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // ✅ Ensures your Angular app on 4200 can talk to Spring on 8081
-        registry.addMapping("/api/**")
+         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
